b2a7ef69268bc1dcde485a3999049c51506a7492
Ut0chka <99210119+Ut0chka@users.noreply.github.com>
2022-02-08 00:57:11 +0300

    Update odd.py

--- odd.py
+++ odd.py
@@ -1,29 +1,29 @@
-# from os import getcwd
-# import sys
-# import os
-# import time
-#
-# print(sys.version)
-# print(os.getenv('APPDATA'))
-# print(time.strftime("%I : %M %p %A"))
-# from datetime import datetime
-# import time
-# import random
-#
-# odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 16, 17, 19,
-#         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
-#         41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
-#
-#
-# for i in range(5):
-#     right_this_minute = datetime.today().minute
-#     if right_this_minute in odds:
-#         print("This minute seems a little odd.")
-#     else:
-#         print("Not an odd minute")
-#     wait_time = random.randint(1,60)
-#     print(wait_time)
-#     time.sleep(wait_time)
+from os import getcwd
+import sys
+import os
+import time
+
+print(sys.version)
+print(os.getenv('APPDATA'))
+print(time.strftime("%I : %M %p %A"))
+from datetime import datetime
+import time
+import random
+
+odds = [ 1, 3, 5, 7, 9, 11, 13, 15, 16, 17, 19,
+        21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
+        41, 43, 45, 47, 49, 51, 53, 55, 57, 59 ]
+
+
+for i in range(5):
+    right_this_minute = datetime.today().minute
+    if right_this_minute in odds:
+        print("This minute seems a little odd.")
+    else:
+        print("Not an odd minute")
+    wait_time = random.randint(1,60)
+    print(wait_time)
+    time.sleep(wait_time)
 
 name = 'John'
 print(f'''hfjfkhkf
